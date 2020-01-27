f16b54b21439723d53473e2a5401a08de0d80b29
Naveen G <41387473+AquamanRanda@users.noreply.github.com>
2020-01-27 19:09:42 +0530

    Flask fully done

--- flask_application.py
+++ flask_application.py
@@ -36,7 +36,14 @@ def get_featured(course):
             continue
         links.append(link.get('href'))
     return {feat :links[0]}
-
+def create_dict(courses,links):
+    res = {} 
+    for key in courses: 
+        for value in links: 
+            res[key] = value
+            links.remove(value) 
+            break  
+    return res
 
 def get_paid3(title):
     links = []
@@ -53,13 +60,7 @@ def get_paid3(title):
     for i in range(0,3):
         clean = remove_tags(str(top[i]))
         courses.append(clean)
-    res = {} 
-    for key in courses: 
-        for value in links: 
-            res[key] = value
-            links.remove(value) 
-            break  
-    return res
+    return create_dict(courses,links)
     
 def get_free3(title):
     links = []
@@ -76,43 +77,43 @@ def get_free3(title):
     for i in range(0,3):
         clean = remove_tags(str(top[i]))
         courses.append(clean)
-    res = {} 
-    for key in courses: 
-        for value in links: 
-            res[key] = value
-            links.remove(value) 
-            break  
-    return res
+    return create_dict(courses,links)
+    
+def get_list():
+    names = []
+    links = []
+    URL = "https://hackr.io"
+    soup = BeautifulSoup(get(URL),"lxml")
+    for link in soup.findAll('a',{'id','topic-grid-item js-topic-grid-item'}):
+        if link.get('href').startswith("/"):
+            links.append(link.get('href'))
+            continue
+        links.append(link.get('href'))
+    for i in range(len(links)):
+        a = links[i]
+        names.append(a[33:])
+    return names
 
-'''if __name__ == '__main__':
-    titl = input("Enter the course: ")
-    title = titl.replace(" ","-")
-    Main_URL = "https://hackr.io/tutorials/learn-"+title
-    course = input("Free/Paid ?")
-    if(course == 1):
-        URL = "https://hackr.io/tutorials/learn-"+title+"?sort=upvotes&type_tags%5B%5D=1&languages%5B%5D=en"
-    else:
-        URL = "https://hackr.io/tutorials/learn-"+title+"?sort=upvotes&type_tags%5B%5D=2&languages%5B%5D=en"
-    url = get_url(URL)
-    content = get(URL)
-    get_featured(title)
-    get_top3(url,content)'''
 def Merge(dict1, dict2): 
     res = {**dict1, **dict2} 
     return res 
 
 app = Flask(__name__)
-
-#languages = [{'name' : 'python'} , {'name' : 'javascript'}, {'name' : 'android-development'}, {'name' : 'java'} , {'name' : 'data-structures-algorithms'} , {'name': 'c-plus-plus'}, {'name': 'react'} , {'name': 'angular'} ,{'name': 'html-5'},{'name':'css'} ,{'name':'c'},{'name':'git'},{'name':'bootstrap'} ,{'name': 'c-sharp'} ,{'name': 'information-security-ethical-hacking'} , {'name': 'blockchain-programming'} ,{'name': 'django'}, {'name': 'mysql'},{'name': 'sql'} , {'name': 'arduino'}, {'name': 'unity'}, {'name' : 'react-native'} , {'name': 'jquery'} , {'name' :'intro-to-programming'}, {'name':'vue-js'},{'name': 'bitcoin'} , {'name': 'go'}, {'name' :'flutter'}, {'name' :'ios-swift'}, {'name' :'asp-net'} ,{'name' :'kotlin'} , {'name': 'typescript'}]
+a = get_list()
+languages = []
+for i in range(len(a)):
+    languages.append({'name': a[i]})
 
 @app.route('/', methods=['GET'])
-def test():
+def lists():
+    return jsonify({'programs': languages})
+@app.route('/<string:name>', methods=['GET'])
+def test(name):
     if request.method == 'GET':
-        course = input("Enter a course: ")
-        feat = get_featured(course)
-        paid = get_paid3(course)
-        free = get_free3(course)
-        return jsonify({'program': {course : {'featured': feat,'paid_top3' : paid,'free_top3': free}}})
+        feat = get_featured(name)
+        paid = get_paid3(name)
+        free = get_free3(name)
+        return jsonify({'program': {name : {'featured': feat,'paid_top3' : paid,'free_top3': free}}})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(debug=True, port=8911)
